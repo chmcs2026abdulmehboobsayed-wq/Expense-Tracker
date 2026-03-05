0737e9ee25acd5de6b3f2f83ba87459b9d3b5134
chmcs2026abdulmehboobsayed-wq <chmcs2026.abdulmehboob.sayed@gmail.com>
2026-03-05 16:34:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://expense-tracker-dloi.onrender.com/expenses",
+  baseURL: "https://expense-tracker-dloi.onrender.com",
 });
 
 export default axiosInstance;
